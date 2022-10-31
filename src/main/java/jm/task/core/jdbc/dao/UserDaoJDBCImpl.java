266c72929cebbb3685341bed7c4b7e8ae930f324
medjidi <medjidi@yandex.ru>
2022-10-31 21:51:25 +0300

    PleaseJdbc

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    Connection connection = Util.getConnection();
+    Connection connection;
 
 
     public UserDaoJDBCImpl() {
@@ -16,10 +16,11 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-
+        connection = Util.getConnection();
         try (Statement statement = connection.createStatement()) {
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS users (id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(40), lastName VARCHAR(40), age TINYINT)");
             connection.commit();
+            connection.close();
         } catch (SQLException e) {
             System.out.println("Database has not been created");
         }
@@ -27,54 +28,63 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void dropUsersTable() {
-
+        connection = Util.getConnection();
         try (Statement statement = connection.createStatement()) {
             statement.executeUpdate("DROP TABLE IF EXISTS users");
             connection.commit();
-
+            connection.close();
         } catch (SQLException e) {
             System.out.println("Database has not been dropped");
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        connection = Util.getConnection();
         try (PreparedStatement statement = connection.prepareStatement("INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)")) {
             statement.setString(1, name);
             statement.setString(2, lastName);
             statement.setInt(3, age);
             statement.executeUpdate();
-            statement.getConnection().commit();
+            connection.commit();
+            connection.close();
         } catch (SQLException e) {
             System.out.println("Database has not been increase");
         }
     }
 
     public void removeUserById(long id) {
+        connection = Util.getConnection();
         try (Statement statement = connection.createStatement()) {
             statement.executeUpdate("DELETE FROM users WHERE id = id");
             connection.commit();
+            connection.close();
         } catch (SQLException e) {
             System.out.println("Database has not been cleared");
         }
     }
 
     public List<User> getAllUsers() {
+        connection = Util.getConnection();
         List<User> list = new ArrayList<>();
-        try (ResultSet resultSet = connection.prepareStatement("SELECT * FROM users").executeQuery("SELECT * FROM users")) {
+        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM users")) {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
             while (resultSet.next()) {
                 list.add(new User(resultSet.getLong(1), resultSet.getString(2), resultSet.getString(3), resultSet.getByte(4)));
             }
+            resultSet.close();
+            connection.close();
         } catch (SQLException e) {
             System.out.println("Database has not been read");
         }
         return list;
     }
 
-    public void cleanUsersTable() {
+    public void cleanUsersTable() {        connection = Util.getConnection();
+        connection = Util.getConnection();
         try (Statement statement = connection.createStatement()) {
             statement.executeUpdate("TRUNCATE users");
             connection.commit();
+            connection.close();
         } catch (SQLException e) {
             System.out.println("Database has not been cleared");
         }
